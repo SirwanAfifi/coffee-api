c131dfd02cf23693b44bfc89e249881c398e10eb
Sirwan <sir1afifi@gmail.com>
2020-12-22 11:10:33 +0330

    Add dynamic endpoints

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('/flavors')
-  findAll() {
-    return 'This returns all coffees';
+  @Get()
+  findAll(@Res() response) {
+    //return 'This returns all coffees';
+    return response.status(200).send({ name: 'Hi' });
+  }
+
+  @Get(':id')
+  findOne(@Param() params) {
+    return `This action returns #${params.id} coffee`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.OK)
+  create(@Body() body) {
+    return body;
   }
 }
